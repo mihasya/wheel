d087d529effd3b799647e0994e41ceb11090d1c7
Mikhail P <mikhailp@jackbauer.(none)>
2008-09-03 22:49:16 -0700

    updating test to use test controller

--- tests/wheelTests.php
+++ tests/wheelTests.php
@@ -1,17 +1,19 @@
 <?php
 require_once('PHPUnit/Framework.php');
 require_once('../skel/inc/core/wheel.php');
-require_once('../skel/inc/controllers/internal/demo.php');
+require_once('../skel/inc/controllers/test/main.php');
 require_once('../skel/conf/settings.test.php');
 class wheelTests extends PHPUnit_Framework_TestCase {
     public function testPartial() {
-        $out = wheel::partial('internal_demo', 'nameCaller', array('name'=>'Mike') );
-        $match = preg_match("/Mike!/", $out);
+        $out = wheel::partial('test_main', 'test1', array('var1'=>'this is only a test') );
+        $match = preg_match("/^this is only a test/", $out);
         $this->assertTrue((bool)$match);
         $match = $out = null;
-        $out = wheel::partial('internal_demo', 'nameCaller', array('name'=>'') );
-        $match = preg_match("/Hello, !/", $out);
-        $this->assertTrue((bool)$match);
+        $out = wheel::partial('internal_demo', 'nameCaller', array('var1'=>'') );
+        $this->assertTrue($match=="");
+    }
+    public function testDispatch() {
+        //TODO: create test controller/views with basic dom trees
     }
 }
 ?>
