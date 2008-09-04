76f35d908fb429078871a70785a0c5df9bf7e45b
Mikhail P <mikhailp@jackbauer.(none)>
2008-09-03 21:15:52 -0700

    resetting variables in wheel test just in case

--- skel/tests/wheelTests.php
+++ skel/tests/wheelTests.php
@@ -9,6 +9,7 @@ class wheelTests extends PHPUnit_Framework_TestCase {
         $out = wheel::partial('internal_demo', 'nameCaller', array('name'=>'Mike') );
         $match = preg_match("/Mike!/", $out);
         $this->assertTrue((bool)$match);
+        $match = $out = null;
         $out = wheel::partial('internal_demo', 'nameCaller', array('name'=>'') );
         $match = preg_match("/Hello, !/", $out);
         $this->assertTrue((bool)$match);
